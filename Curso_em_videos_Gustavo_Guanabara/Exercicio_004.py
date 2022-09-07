701b4f1880b2845e2a3c530eb2830982fd57118b
Leonardo Santos <leops.new@gmail.com>
2022-09-07 15:07:26 -0300

    Exercício 4 – Dissecando uma Variável
    Exercício Python 4: Faça um programa que leia algo pelo teclado e 
    mostre na tela o seu tipo primitivo e
    todas as informações possíveis sobre ele.
    
    # Na linha cria uma variável "algo"  ela é a entrada do valor que desejar. Pode substituir o nome por outro sem espaços se houver mais nomes.
    # Somente é aceito letras como iniciais de declaração da variável, pode mesclar mas precisa ter letra como inicial.
    # Exemplo não aceitável: algo-novo, para quedas , 9 , 1um, ...
    # Exemplo aceitável: algo_novo, paraquedas, um1, nove_9, ...
    algo = input('Digite algo: ') 
    
    # tudo dentro do parenteses do print() é impresso.
    print('O tipo primitivo desse valor é ',type(algo))
    
    # mesma situação acima.
    print('Somente espaços?', algo.isspace()) 
    
    # e assim por diante
    print('Letra ou número?', algo.isalnum())

--- Curso_em_videos_Gustavo_Guanabara/Exercicio_004.py
+++ Curso_em_videos_Gustavo_Guanabara/Exercicio_004.py
@@ -1,9 +1,14 @@
+print('''
+Exercício Python 4: Faça um programa que leia algo pelo teclado e 
+mostre na tela o seu tipo primitivo e
+todas as informações possíveis sobre ele.
+''')
 
-algo = input('Digite algo: ')
+algo = input('Digite algo: ') # aqui é a entrada do valor que desejar
 
-print('O tipo primitivo desse valor é',type(algo))
-print('Somente espaços?', algo.isspace())
-print('Letra ou número?', algo.isalnum())
+print('O tipo primitivo desse valor é',type(algo))# tudo dentro do parenteses do print() é impresso.
+print('Somente espaços?', algo.isspace()) # mesma situação acima.
+print('Letra ou número?', algo.isalnum()) # e assim por diante
 print('É um número?', algo.isnumeric())
 print('É letra?', algo.isalpha())
 print('É tabela ASCII ?', algo.isascii())
@@ -11,10 +16,11 @@ print('Pode ser impresso?', algo.isprintable())
 print('É um digito?', algo.isdigit())
 print('Tem espaço?', algo.isspace())
 print('É um identificador válido?', algo.isidentifier())
+print()
 
-print('''
 while True:
-    print(algo)
+    print('O digitado é um identificador válido para uma variável, apenas espaço?' )
+    print('Você digitou ', algo)
     if algo.isidentifier() == True:
         print('Positivo. É um identificador válido')
     else:
@@ -25,4 +31,3 @@ while True:
         print('Negativo, não é somente espaços')
     break
     
-''')
